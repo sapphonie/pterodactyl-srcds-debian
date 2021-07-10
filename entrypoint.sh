a4942f48be6f58ff425a881925bd9e198d9f237e
steph <thesqrtminus1@gmail.com>
2021-07-09 20:56:24 -0400

    more bash tweaks
    hopefully will fix fuckery with docker entrypoints, garbage software lol

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-cd /home/container
+cd /home/container || exit 1
 sleep 1
 # Make internal Docker IP address available to processes.
 INTERNAL_IP=$(ip route get 1 | awk '{print $NF;exit}')
 export INTERNAL_IP
 
-## just in case someone removed the defaults.
-if [ "${STEAM_USER}" == "" ]; then
-    echo -e "steam user is not set.\n"
-    echo -e "Using anonymous user.\n"
-    STEAM_USER=anonymous
-    STEAM_PASS=""
-    STEAM_AUTH=""
-else
-    echo -e "user set to ${STEAM_USER}"
-fi
 
+# run bash if we're running in a setup script, otherwise, run normal server logic
+[[ $1 =~ bash ]] && runbash || runserver
 
-# Replace Startup Variables
-MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
-echo -e ":/home/container$ ${MODIFIED_STARTUP}"
+runbash()
+{
+    bash
+}
 
-# Run the Server
-eval ${MODIFIED_STARTUP}
+runserver()
+{
+    # Replace Startup Variables
+    MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+    echo -e ":/home/container$ ${MODIFIED_STARTUP}"
+
+    # Run the Server
+    eval ${MODIFIED_STARTUP}
+}
